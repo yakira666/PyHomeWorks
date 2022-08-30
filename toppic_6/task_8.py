fe70ffdd3cc6c71c711fe8e30c0391a8bf12bb6e
Kirill <zebzeeff.kirill@yandex.ru>
2022-08-30 23:25:09 +0300

    поправил на elif

--- toppic_6/task_8.py
+++ toppic_6/task_8.py
@@ -8,21 +8,21 @@ if mounth == "Март" or mounth == "Апрель" or mounth == "Май" or mou
         print(mounth, day, "Соответствуют сезону Весна")
     elif mounth == "Июнь" and 0 < day <= 20:
         print(mounth, day, "Соответствуют сезону Весна")
-if mounth == "Июнь" or mounth == "Июль" or mounth == "Август" or mounth == "Сентябрь":
+elif mounth == "Июнь" or mounth == "Июль" or mounth == "Август" or mounth == "Сентябрь":
     if (mounth == "Июль" or mounth == "Август") and 0 < day <= 31:
         print(mounth, day, "Соответствуют сезону Лето")
     elif mounth == "Июнь" and 20 < day <= 31:
         print(mounth, day, "Соответствуют сезону Лето")
     elif mounth == "Сентябрь" and 0 < day <= 21:
         print(mounth, day, "Соответствуют сезону Лето")
-if mounth == "Сентябрь" or mounth == "Октябрь" or mounth == "Ноябрь" or mounth == "Декабрь":
+elif mounth == "Сентябрь" or mounth == "Октябрь" or mounth == "Ноябрь" or mounth == "Декабрь":
     if (mounth == "Октябрь" or mounth == "Ноябрь") and 0 < day <= 31:
         print(mounth, day, "Соответствуют сезону Осень")
     elif mounth == "Сентябрь" and 22 <= day <= 31:
         print(mounth, day, "Соответствуют сезону Осень")
     elif mounth == "Декабрь" and 0 < day <= 20:
         print(mounth, day, "Соответствуют сезону Осень")
-if mounth == "Декабрь" or mounth == "Январь" or mounth == "Февраль" or mounth == "Март":
+elif mounth == "Декабрь" or mounth == "Январь" or mounth == "Февраль" or mounth == "Март":
     if (mounth == "Январь" or mounth == "Февраль") and 0 < day <= 31:
         print(mounth, day, "Соответствуют сезону Зима")
     elif mounth == "Декабрь" and 20 < day <= 31:
