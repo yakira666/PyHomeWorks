4dddd061bd6b75e3753f3b248daf26f79df4a684
Kirill <zebzeeff.kirill@yandex.ru>
2022-09-05 00:01:31 +0300

    ДЗ переделал, делаю спец. задание

--- toppic_7/task_7.py
+++ toppic_7/task_7.py
@@ -1,9 +1,14 @@
 num = int(input("Введите целое число: "))
-for i in range(num, 0, -1):
-    for j in range(num):
-        a = i - j
-        if a > 0:
-            print(a, "", end="")
-    print()
+# for i in range(num, 0, -1):
+#     for j in range(num):
+#         a = i - j
+#         if a > 0:
+#             print(a, "", end="")
+#     print()
 # Неверное решение задачи
 # Как можно решить эту задачу, без использования условного оператора и без допольнительной переменной
+
+for i in range(num, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
--- toppic_7/task_8.py
+++ toppic_7/task_8.py
@@ -1,9 +1,17 @@
 input_stars = int(input("Введите кол-во звезд: "))
-for i in range(input_stars):
-    print("* " * i)
-for i in range(input_stars, -1, -1):
-    print("* " * i)
+# for i in range(input_stars):
+#     print("* " * i)
+# for i in range(input_stars, -1, -1):
+#     print("* " * i)
 
 # Неправильный подход к решению задачи, после последней строки пустая лишняя строка
-
 # Подсказка: Задача решается с помощью вложенных циклов
+
+for i in range(input_stars):
+    for k in range(0, i + 1):
+        print("*", end=" ")
+    print()
+for i in range(input_stars-1, 0, -1):
+    for k in range(i, 0, -1):
+        print("*", end=" ")
+    print()
