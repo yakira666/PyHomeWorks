56a89babaacc3fdb10212c5bfee4233b65da4e22
Kirill <zebzeeff.kirill@yandex.ru>
2022-09-05 17:11:21 +0300

    доделлал спец задание под нужную форму

--- spec_works/special.py
+++ spec_works/special.py
@@ -9,24 +9,23 @@
 #     v -= 1
 
 N = int(input("Введите число: "))  # 4
-# for i in range(N, 0, -1):
-#     # пустые пробелы перед символами строки
-#     for j in range(i, 0, -1):
-#         print(' ', end='')
-#
-#     # символы строки
-#     for k in range(i, N+1, 1):
-#         print('**', end='')
-#         # переход на новую строку
-#     print()
-#
-# for i in range(1, N+1, 1):
-#     # пустые пробелы перед символами строки
-#     for j in range(1, i+1, 1):
-#         print(' ', end='')
-#
-#     # символы строки
-#     for k in range(N, i-1, -1):
-#         print('**', end='')
-#     print()
+for i in range(N, 0, -1):
+    # пустые пробелы перед символами строки
+    for j in range(i, 0, -1):
+        print(' ', end='')
 
+    # символы строки
+    for k in range(i, N + 1, 1):
+        print('**', end='')
+        # переход на новую строку
+    print()
+
+for i in range(1, N + 1, 1):
+    # пустые пробелы перед символами строки
+    for j in range(1, i + 1, 1):
+        print(' ', end='')
+
+    # символы строки
+    for k in range(N, i - 1, -1):
+        print('**', end='')
+    print()
