2a4d5d3908c2ff9bc9ddbd2e89490d7695be2a71
Shoxrux Yuldashov <deagleshox@mail.ru>
2022-09-06 04:27:30 +0300

    Шаблон
    - Добавил свое решение

--- spec_works/special.py
+++ spec_works/special.py
@@ -29,3 +29,27 @@ for i in range(1, N + 1, 1):
     for k in range(N, i - 1, -1):
         print('**', end='')
     print()
+
+
+# мой вариант
+N = int(input("Введите число: "))
+
+for i in range(N, 0, -1):
+    for j in range(i):
+        print(' ', end='')
+
+    for k in range(i, N + 1):
+        print('**', end='')
+    print()
+
+# вместо for i in range(1, N + 1, 1): я бы сделал как в первом цикле for i in range(N, 0, -1):
+for i in range(N, 0, -1):
+
+    # сюда просто вставлю вложенные циклы из первого внешнего цикла и переставлю условия вложенных циклов
+    # так как, мне нужно построить такой же треугольник, только в обратном направлении
+    for j in range(i, N + 1):
+        print(' ', end='')
+
+    for k in range(i):
+        print('**', end='')
+    print()
