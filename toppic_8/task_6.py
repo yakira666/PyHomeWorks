ff4d09f0da9063f1ed994680ff079f8ef04ff633
Shoxrux Yuldashov <deagleshox@mail.ru>
2022-09-13 04:08:56 +0300

    ДЗ 8
    - Правильное решение!

--- toppic_8/task_6.py
+++ toppic_8/task_6.py
@@ -1,40 +1,14 @@
 start_range = int(input("Начало диапозона: "))
 finish_range = int(input("Конец диапозона: "))
-print("Простые число от", start_range, "до", finish_range)
-for i in range(start_range, finish_range + 1):
 
-    # вместо двух условий можно применить одно условие, но какое?
-    # if start_range == 0:
-    #     i += 2
-    # elif start_range == 1:
-    #     i += 1
+print("Простые число от", start_range, "до", finish_range)
+
+for i in range(start_range, finish_range + 1):
     if i > 1:
         for j in range(2, i):
             if i % j == 0:
                 break
         else:
             print(i)
-'''
-# Твое решение
-Начало диапозона: 0
-Конец диапозона: 7
-Простые число от 0 до 7
-0
-1
-2
-3
-5
-7
 
-
-# моё решение
-Начало диапазона: 0
-Конец диапазона: 7
-Простые числа от 0 до 7
-2
-3
-5
-7
-
-0 и 1 не являются простыми числами
-'''
+# Отлично!
