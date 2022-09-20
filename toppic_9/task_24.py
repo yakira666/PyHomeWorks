739ea8b6a9e54c864b2e0ec17b343b71b871f57a
Kirill <zebzeeff.kirill@yandex.ru>
2022-09-21 01:11:46 +0300

    Доделал задачи по 9 теме

--- toppic_9/task_24.py
+++ toppic_9/task_24.py
@@ -1,25 +1,37 @@
-data = input("Введите любую строку (оставьте пустым для выхода): ")
-data_chars = "\'\")%$&/*^:;/\\|#]}@!?[{<>."
-
-while data != "":
-    print(f"\n\t\t\t\t\tВ строке \"{data}\"")
-    print(f'Пробелы: {data.count(" ")}')
-    print(f'Прописные буквы: {sum(i.isupper() for i in data)}')
-    print(f'Строчные буквы: {sum(i.islower() for i in data)}')
-    print(f'Числа: {sum(i.isdigit() for i in data)}')
-    print(f'Специальные строки: {sum(i in data_chars for i in data)}\n')
-    data = input("Введите любую строку (оставьте пустым для выхода): ")
-
+char_space = 0
+char_upper = 0
+char_lower = 0
+char_digit = 0
+char_symbol = 0
+n = "\t"  # Сделал так тк никогда не знаю точную длину вводной строки
+while data := input("Введите любую строку (оставьте пустым для выхода): "):
+    print(f'{n * 5}В строке \"{data}\"')
+    for i in data:
+        if i == " ":
+            char_space += 1
+        elif i.isupper():
+            char_upper += 1
+        elif i.islower():
+            char_lower += 1
+        elif i.isdigit():
+            char_digit += 1
+        else:
+            char_symbol += 1
+    print(f'Пробелы: {char_space}\n'
+          f'Прописные буквы: {char_upper}\n'
+          f'Строчные буквы: {char_lower}\n'
+          f'Числа: {char_digit}\n'
+          f'Специальные символы: {char_symbol}\n')
+    # data_chars = "\'\")%$&/*^:;/\\|+-_(]}@!?[{<=>,.`"
+    # print(f"В строке \"{data: >20}\"")
+    # print(f'Пробелы: {data.count(" ")}')
+    # print(f'Прописные буквы: {sum(i.isupper() for i in data)}')
+    # print(f'Строчные буквы: {sum(i.islower() for i in data)}')
+    # print(f'Числа: {sum(i.isdigit() for i in data)}')
+    # print(f'Специальные строки: {sum(i in data_chars for i in data)}\n')
+'''
+ДЛЯ ТЕСТА
 
 # Это строка содержит числа 50 & 160 & 234 & 3 & 1. Специальные символы `))"%$
 # J20WF $ W10|ikz* ?poqde6u ?.Ieays.VC %B2<{= S**
-
-
 '''
-1. Не оптимальное решение задачи
-
-2. Есть баг, если вводить данные со скриншота, то выдает неверный результат
-
-3. Ещё попробуй использовать оператора "моржа"
-'''
-
