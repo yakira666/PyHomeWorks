8d248001a6776332d8b74fa17057242d0b602cdd
Shoxrux Yuldashov <deagleshox@mail.ru>
2022-10-08 18:25:52 +0300

    ДЗ 11.1
    - Правильное решение: №1, №2, №3, №4, №5, №6, №7, №8
    
    - Исправление: №4, №7, №8
    
    - Неверное решение:
    
    - На доработку:

--- toppic_11/task_4.py
+++ toppic_11/task_4.py
@@ -3,5 +3,11 @@
 # print("Изменненый кортеж: ", (tuple(data_given)))
 
 data_given = (4, 6, 2, 8, 3, 1)
-data = ("10", )
-print(data_given+data)
+# data = ("10", )
+# print(data_given+data)
+
+print(f'Исходный кортеж: {data_given}')
+user_inp = input('Введите элемент для добавления в кортеж: ')
+print('Изменённый кортеж:', data_given + (user_inp,))
+
+# Добавлена возможность получить значение от пользователя и добавить его в конец кортежа
--- toppic_11/task_8.py
+++ toppic_11/task_8.py
@@ -2,14 +2,15 @@
 # data = (2, 4, 5, 6, 2, 3, 4, 4, 7)
 data = [int(i) for i in
         input("Введите исходный кортеж: ")
-        .replace(" ", "")
         .replace(' ', "")
-        .replace("(", "")
-        .replace(")", "")
-        .replace(",", "").strip("()")]
+        .replace(",", "")
+        .strip(" ()")]
+
 print(f"Исходный кортеж: {tuple(data)}")
 char_count = int(input("Укажите искомый элемент: "))
 if char_count in data:
     print(data.count(char_count))
 else:
     print(f"\"{char_count}\" нет в кортеже")
+
+# Вместо замены круглых скобок лучше использовать strip
