9034d89039e505551d056e31ff62b6c05452adeb
Kirill <zebzeeff.kirill@yandex.ru>
2022-10-12 17:36:14 +0300

    Revert "исправил решение полностью!"
    This reverts commit 57c005511b5d5fe1394ee536597ff3974a898ebf.

--- toppic_11/task_9.py
+++ toppic_11/task_9.py
@@ -1,13 +1,13 @@
-# data = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
-# print(f'Исходный список кортежей: {data}')
-# num = int(input('Укажите значение: '))
-# # # №1
-# # for item in data:
-# #     print(item[:-1]+(num, ))
-# # # №2
-# print(f'Измененный список кортежей: {[item[:-1] + (num,) for item in data]}')
-#
-# # # № 3
+data = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+print(f'Исходный список кортежей: {data}')
+num = int(input('Укажите значение: '))
+# # №1
+# for item in data:
+#     print(item[:-1]+(num, ))
+# # №2
+print(f'Измененный список кортежей: {[item[:-1] + (num,) for item in data]}')
+
+# # № 3
 # data_2 = [[int(j) for j in data[i]]for i in range(len(data))]
 # for i in range(len(data_2)):
 #     for j in range(len(data_2[i])):
@@ -15,6 +15,3 @@
 # print(list(tuple(i) for i in data_2))
 
 # # № 4 Доделать на инпут
-"""
-Убрать пробелы по бокам и [] и разбивать по запятым -> работа со списками из кортежей
-"""
