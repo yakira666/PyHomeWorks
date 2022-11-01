dac5e09ddba3ae5f211e1c0c1e3087efe763762d
Kirill <zebzeeff.kirill@yandex.ru>
2022-11-02 01:56:52 +0300

    Добавил создание списков

--- toppic_13/task_3.py
+++ toppic_13/task_3.py
@@ -1,25 +1,30 @@
-scrabble = {1: ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"],
-            2: ["D", "G"],
-            3: ["B", "C", "M", "P"],
-            4: ["F", "H", "V", "W", "Y"],
-            5: ["K"],
-            8: ["J", "X"],
-            10: ["Q", "Z"]
-            }
+chars = [chr(char) for char in range(65, 91)]
+dictionary = {}
+for i in range(len(chars)):
+    if chars[i] in "AEILNORSTU":
+        dictionary[chars[i]] = 1
+    elif chars[i] in "DG":
+        dictionary[chars[i]] = 2
+    elif chars[i] in "BCMP":
+        dictionary[chars[i]] = 3
+    elif chars[i] in "FHVWY":
+        dictionary[chars[i]] = 4
+    elif chars[i] in "K":
+        dictionary[chars[i]] = 5
+    elif chars[i] in "JX":
+        dictionary[chars[i]] = 8
+    elif chars[i] in "QZ":
+        dictionary[chars[i]] = 10
 
 data = input("Введите слово: ")
-
 data_s = data.upper()
-counter = 0
+
+# №1
+score = 0
 for i in data_s:
-    for k in scrabble:
-        if i in (scrabble.get(k)):
-            counter += k
+    score += dictionary.get(i, 0)
 
-print(f"{data} оценивается в {counter} очков!")
+print(f"{data} оценивается в {score} очков!")
 
-# Задача решена неправильно
-# Реализованный алгоритм решения не является производительным
-# Разбери шаг за шагом ((можно использовать отладку), как работает программа,
-# и подумайте, как можно улучшить производительность программы
-
+# № 2
+print(f"{data} оценивается в {sum([(dictionary.get(i, 0)) for i in data_s])} очков!")
