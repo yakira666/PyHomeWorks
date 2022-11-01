3100f393ed7da143f9e395d7ef4b67ead75e92ee
Kirill <zebzeeff.kirill@yandex.ru>
2022-11-01 22:52:28 +0300

    Решил генератором словарей

--- toppic_13/task_2.py
+++ toppic_13/task_2.py
@@ -1,13 +1,2 @@
 data = input().upper()
-
-set_data = set(data)
-frst_dict = dict()
-
-for i in set_data:
-    frst_dict.setdefault(i, data.count(i))
-print(frst_dict)
-
-# Отлично!
-
-# Эту задачу нужно будет решить другим способом (используя генератор словарей),
-# после того, как мы посмотрим генератор словарей
+print({k: data.count(k) for k in data})
